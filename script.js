2df52a7158be7a4ac86f600d70cd5e7696820288
Sidharth0999 <148868058+Sidharth0999@users.noreply.github.com>
2024-10-04 00:21:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('.nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
